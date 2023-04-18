dfa8690c1d4c191d5cb1c0bf6dcdd1982d6edabc
emansamir2 <51717395+emansamir2@users.noreply.github.com>
2023-04-18 17:39:18 +0200

    Update fixed-point.h

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -12,3 +12,14 @@
 #define DIV_REAL_INT(x,n) (x)/(n)
 #define MUL_REAL(x,y) ((int64_t)x)*(y)/(F)
 #define DIV_REAL(x,y) ((int64_t)x)*(F)/(y)
+/*
+note:
+int-> priority , nice , ready_threads
+
+real->recent_cpu , load_avg
+
+
+recent_cpu=ADD_REAL_INT(MUL_REAL(DIV_REAL((2*load_avg),ADD_REAL_INT((2*load_avg),1)),recent_cpu),nice);
+priority=PRI_MAX-TO_NEARINT(recent_cpu/4)-(nice*2);
+load_avg=MUL_REAL(DIV_REAL(TO_REAL(59),TO_REAL(60)),load_avg)+DIV_REAL(TO_REAL(1),TO_REAL(60))*ready_threads
+*/
