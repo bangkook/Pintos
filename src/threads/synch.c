04834f4cef6bb63fd362e5fe2c6564e5eebadc03
bangkook <m.hossam2551@gmail.com>
2023-05-21 22:48:22 +0300

    Check not inter_context in sema up

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -132,7 +132,7 @@ sema_up (struct semaphore *sema)
   }
   sema->value++;
 
-  if(t != NULL && t->priority > thread_current()->priority)
+  if(!intr_context() && t != NULL && t->priority > thread_current()->priority)
     thread_yield();  
 
   intr_set_level (old_level);
